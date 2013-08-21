5f8109b453ccd602fe0261e217f8e492458acbdf
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-08-20 19:23:10 -0500

    Dealing logic

--- main.rb
+++ main.rb
@@ -4,8 +4,39 @@ require 'sinatra'
 set :sessions, true
 
 get '/' do 
-  erb :home
+  if session[:player_name]
+  	redirect '/game'
+  else
+  	redirect '/new_player'
+  end
 end
+
+get '/new_player' do
+  erb :new_player
+end
+
+post '/new_player' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do
+	# deck
+	suits = ['H', 'D', 'S', 'C']
+	values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+	session[:deck] = suits.product(values).shuffle! #[['H', '9'], ['C', 'K']]
+
+	#deal cards
+	session[:dealer_cards] = []
+	session[:player_cards] = []
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop
+  erb :game
+end
+
+
 
 get '/about' do
 	erb :about
@@ -14,4 +45,3 @@ end
 get '/contact' do
 	erb :contact
 end
-
