d7881e29ab5a3517b29ceb334d21d6f29b4933a0
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-08-27 20:11:53 -0500

    More ajax

--- public/application.js
+++ public/application.js
@@ -1,16 +1,47 @@
 $(document).ready(function() {
-  // $("form#hit_form input").click(function(){
+  player_hits();
+  player_stays();
+  dealer_hit();
+});
 
-  $(document).on('click', 'form#hit_form input', function() {
+function player_hits() {
+  $(document).on("click", "form#hit_form input", function() {
     alert("Player hits!");
 
     $.ajax({
-      type: 'POST',
-      url: '/game/player/hit',
+      type: "POST",
+      url: "/game/player/hit",
     }).done(function(msg) {
-      $("#game").replaceWith(msg);
+      $("#game").replaceWith(msg)
     });
-
     return false;
   });
-});
+}
+
+function player_stays() {
+  $(document).on("click", "form#stay_form input", function() {
+    alert("Player stays!");
+
+    $.ajax({
+      type: "POST",
+      url: "/game/player/stay",
+    }).done(function(msg) {
+      $("#game").replaceWith(msg)
+    });
+    return false;
+  });
+}
+
+function dealer_hit() {
+  $(document).on("click", "form#dealer_hit input", function() {
+    alert("Dealer hits!");
+
+    $.ajax({
+      type: "POST",
+      url: "/game/dealer/hit",
+    }).done(function(msg) {
+      $("#game").replaceWith(msg)
+    });
+    return false;
+  });
+}
